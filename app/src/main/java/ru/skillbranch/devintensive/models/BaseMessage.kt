9eb9bdf33a5e833c6b185972d34bc98eb1162b91
Анастасия Федорова <fedorova@oe-it.ru>
2020-09-15 00:50:08 +0300

    completed

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -18,8 +18,8 @@ abstract class BaseMessage (
             from: User?,
             chat: Chat,
             date: Date = Date(),
-            type: String = "text",
             payload: Any?,
+            type: String = "text",
             isIncoming: Boolean = false
         ): BaseMessage {
             lastId++
